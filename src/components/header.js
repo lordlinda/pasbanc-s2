2494081ffc06ccc1f1a6fd65f9cc723c222a5d20
lord <marialindananono@gmail.com>
2021-01-04 08:17:24 +0300

    navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,43 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PageLinks from "../constants/links"
-import { FaAlignRight } from "react-icons/fa"
+import { FaAlignJustify } from "react-icons/fa"
 import { Link } from "gatsby"
 import "./header.css"
-const Header = ({ siteTitle, toggleSidebar }) => (
-  <nav className="header">
-    <div className="header__center">
-      <p>
-        <Link to="/">
-          <span className="header__logoSectionOne">pas</span>
-          <span className="header__logoSectionTwo">banc</span>
-        </Link>
-      </p>
-      <button type="button" className="toggle-btn" onClick={toggleSidebar}>
-        <FaAlignRight></FaAlignRight>
-      </button>
+const Header = ({ siteTitle, toggleSidebar }) => {
+  const [show, handleShow] = useState(false)
 
-      <PageLinks styleClass="header__links"></PageLinks>
-    </div>
-  </nav>
-)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true)
+      } else {
+        handleShow(false)
+      }
+    })
+    return () => {
+      window.removeEventListener("scroll")
+    }
+  }, [])
+
+  return (
+    <nav className={`header ${show && "header__shadow"}`}>
+      <div className="header__center">
+        <p>
+          <Link to="/">
+            <span className="header__logoSectionOne">pas</span>
+            <span className="header__logoSectionTwo">banc</span>
+          </Link>
+        </p>
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignJustify></FaAlignJustify>
+        </button>
+
+        <PageLinks styleClass="header__links"></PageLinks>
+      </div>
+    </nav>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
