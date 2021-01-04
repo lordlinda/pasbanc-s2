c12e5d25046392289fbc622d77ad568256b1939d
lord <marialindananono@gmail.com>
2021-01-04 08:00:42 +0300

    tabs

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react"
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Box from "@material-ui/core/Box"
@@ -29,29 +28,184 @@ function About() {
   return (
     <Layout>
       <SEO title="About" />
-      <div className="tabs">
-        <Tabs
-          value={value}
-          indicatorColor="primary"
-          textColor="primary"
-          onChange={handleChange}
-          aria-label="disabled tabs example"
-          variant="scrollable"
-          scrollButtons="auto"
-        >
-          <Tab label="What sets Pasbanc apart" wrapped />
-          <Tab label="Vision & Mission" wrapped />
-          <Tab label="Pasbanc values" wrapped />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-          what sets pasbanc account content here
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          vision and mission content here
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          pasbanc values here
-        </TabPanel>
+      <ul class="nav nav-tabs">
+        <li class="active">
+          <a data-toggle="tab" href="#home">
+            What sets Pasbanc apart
+          </a>
+        </li>
+        <li>
+          <a data-toggle="tab" href="#menu1">
+            Vision & Mission
+          </a>
+        </li>
+        <li>
+          <a data-toggle="tab" href="#menu2">
+            Pasbanc values
+          </a>
+        </li>
+      </ul>
+
+      <div class="tab-content">
+        <div id="home" class="tab-pane fade in active">
+          <h3>What sets pasbanc apart</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div id="menu1" class="tab-pane fade">
+          <h3> Vision and mission</h3>
+          <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <div id="menu2" class="tab-pane fade">
+          <h2>Values</h2>
+          <ul id="values">
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+            <li>
+              <a href="">value one</a>
+            </li>
+          </ul>
+          <br />
+          <div class="container">
+            <h1>Value one</h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim
+            similique odio nostrum exercitationem dicta dolor accusamus quos
+            ducimus commodi hic dolorum omnis architecto neque, tenetur harum
+            quidem cum. Maiores?
+            <a class="toolTip" href="#" data-placement="auto">
+              <sup>&#91;1&#93;</sup>
+              <span class="custom">
+                <em>Critical</em>This is just an example of what you can do
+                using a CSS tooltip, feel free to get creative and produce your
+                own!
+              </span>
+            </a>
+            ,<h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            Here are some examples of a{" "}
+            <sup>
+              <a class="toolTip" href="#">
+                &#91;1&#93;
+              </a>
+            </sup>
+            <span class="classic">
+              This is just an example of what you can do using a CSS tooltip,
+              feel free to get creative and produce your own!
+            </span>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+            <h1>Value one</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore,
+              alias, neque. Iure, repellat perspiciatis consectetur. Odio
+              consequuntur magnam, nemo voluptas laudantium temporibus debitis,
+              explicabo! Quos non, accusamus architecto porro rerum.
+            </p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
