9e1da8421cb8060f0f8ade1e1a9240397e64f451
lord <marialindananono@gmail.com>
2021-04-07 11:09:52 +0300

    remove layout.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,6 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Sidebar from "./Sidebar"
 import Footer from "./footer"
-import "./layout.css"
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
